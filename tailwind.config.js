bd69bfdeac7fb2490fb5598f235014b623901838
RajatSharma <withrajatsharma@gmail.com>
2024-01-04 18:47:10 +0530

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    screens:{
+      sm:"478px",
+      md:"770px",
+      lg:"1000px"
+    },
+    extend: {
+      colors:{
+        bdyClr: "rgb(238, 234, 228)",
+      },
+      fontFamily :{
+        roman : "roman",
+        media : "media",
+      } 
+    },
   },
   plugins: [],
 }
